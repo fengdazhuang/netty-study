33abc339723a1ff37b2bbb1d8c61129ab7c46d4f
fengdazhuang <3172029811@qq.com>
2022-10-03 22:42:33 +0800

    first commit

---
+++ src/main/java/com/fzz/netty/Test1.java
@@ -0,0 +1,48 @@
+package com.fzz.netty;
+
+import java.io.FileInputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+
+/**
+ *
+ * ByteBuffer 正确使用姿势
+ * 1. 向 buffer 写入数据，例如调用 channel.read(buffer)
+ * 2. 调用 flip() 切换至**读模式**
+ * 3. 从 buffer 读取数据，例如调用 buffer.get()
+ * 4. 调用 clear() 或 compact() 切换至**写模式**
+ * 5. 重复 1~4 步骤
+ *
+ *
+ * FileInputStream 和 FileOutputStream为单向的，只能读和写
+ * RandomAccessFile既可以读也可以写
+ *
+*/
+public class Test1 {
+    public static void main(String[] args) {
+        try {
+            RandomAccessFile file = new RandomAccessFile("data1.txt", "rw");
+
+            //FileInputStream file = new FileInputStream("data1.txt");
+            FileChannel channel = file.getChannel();
+            ByteBuffer buffer = ByteBuffer.allocate(20);
+            while(true){
+                int len = channel.read(buffer);
+                if(len==-1){
+                    break;
+                }
+                buffer.flip();
+                while(buffer.hasRemaining()){
+                    System.out.print((char)buffer.get());
+                }
+                buffer.clear();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ src/main/java/com/fzz/netty/Test2.java
@@ -0,0 +1,43 @@
+package com.fzz.netty;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 向ByteBuffer中输入数据的两种方法：
+ * 1 channel的read方法，根据设置的容量分批读取文件的数据
+ * 2 buffer的put方法（字节数组）
+ *
+ * 输出ByteBuffer的数据的两种方法“
+ * 1 channel的write方法，写入文件
+ * 2 buffer的get方法
+ */
+
+public class Test2 {
+    public static void main(String[] args) {
+        try {
+            FileChannel channel = new RandomAccessFile("data2.txt", "rw").getChannel();
+            ByteBuffer buffer = ByteBuffer.allocate(20);
+            buffer.put("abcdefg".getBytes());
+            buffer.flip();
+//            while(buffer.hasRemaining()){
+//                System.out.print( (char) buffer.get());
+//            }
+            channel.write(buffer);
+            buffer.clear();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public void encodeAndDecode(){
+        ByteBuffer buffer1 = StandardCharsets.UTF_8.encode("helloWorld");
+        ByteBuffer buffer2 = Charset.defaultCharset().encode("你好");
+        String toString = StandardCharsets.UTF_8.decode(buffer1).toString();
+        System.out.println(toString);
+    }
+}
