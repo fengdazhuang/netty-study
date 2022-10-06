9dda5befe3a04fe6cf12f12e99c2d9437e4d771a
fengdazhuang <3172029811@qq.com>
2022-10-06 23:08:49 +0800

    fourth day

---
+++ src/main/java/com/fzz/netty/hello/HelloClient.java
@@ -0,0 +1,28 @@
+package com.fzz.netty.hello;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.Date;
+
+public class HelloClient {
+    public static void main(String[] args) throws InterruptedException {
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel channel) throws Exception {
+                             channel.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect("localhost",8080)
+                .sync()
+                .channel()
+                .writeAndFlush(new Date()+": hello world");
+
+    }
+}
